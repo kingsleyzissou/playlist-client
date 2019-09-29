fd5050a98665928f36663b1f10066ff1145ebcf7
Gianluca Zuccarelli <zissou@Zissou.local>
2019-09-29 22:30:11 +0100

    GraphQL Queries

---
+++ pages/playlists/_id.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <section class="columns" style="padding: 1em">
+      <div class="column is-12">
+        <div class="card" v-for="(song, index) in playlist.songs" :key="index">
+          <div class="card-content">
+            <p class="title">{{ song.title }}</p>
+            <p class="heading">Artist: {{ song.artist.name }}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import playlistQuery from "~/apollo/queries/playlist";
+
+export default {
+  data() {
+    return {
+      // Initialize an empty restaurants variabkle
+      playlist: {},
+      query: ""
+    };
+  },
+  apollo: {
+    playlist: {
+      prefetch: true,
+      query: playlistQuery,
+      variables() {
+          let id = Number.parseInt(this.$route.params.id);
+          return { id };
+      }
+    }
+  }
+};
+</script>
---
+++ pages/playlists/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <section class="columns" style="padding: 1em">
+      <div class="column is-12">
+        <div class="card" v-for="(playlist, index) in playlists" :key="index">
+          <div class="card-content">
+            <p class="title">{{ playlist.name }}</p>
+            <p class="heading">{{ playlist.songs.length }} songs</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import playlistsQuery from "~/apollo/queries/playlists";
+
+export default {
+  data() {
+    return {
+      // Initialize an empty restaurants variabkle
+      playlists: [],
+      query: ""
+    };
+  },
+  apollo: {
+    playlists: {
+      prefetch: true,
+      query: playlistsQuery
+    }
+  }
+};
+</script>
